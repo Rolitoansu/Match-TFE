0f54c5cc236dfce57a3c31a9f3e9c99b3c5e88a4
Rolitoansu <rolitoansu@outlook.com>
2026-03-30 20:05:06 +0200

    Fix for deployment

--- webapp/src/api/axios.ts
+++ webapp/src/api/axios.ts
@@ -1,8 +1,11 @@
 import type { User } from '../context/AuthContext'
 import axios from 'axios'
 
+const HOST = import.meta.env.HOST || 'http://localhost'
+const PORT = import.meta.env.API_GATEWAY_PORT || 8000
+
 const api = axios.create({
-    baseURL: import.meta.env.API_GATEWAY_URL || 'http://localhost:8000'
+    baseURL: `${HOST}:${PORT}`
 })
 
 let handleAuthSuccess: (user: User) => void = () => {}
